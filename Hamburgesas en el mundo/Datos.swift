2212ae6b62de1f03a0ca0e928ae6a7effa5978a7
byron <a.c.m.7.1@hotmail.com>
2016-10-09 00:29:29 -0500

    Ultima tarea

---
+++ Hamburgesas en el mundo/Datos.swift
@@ -0,0 +1,46 @@
+//
+//  Datos.swift
+//  Hamburgesas en el mundo
+//
+//  Created by Gibreel Martínez on 08/10/16.
+//  Copyright © 2016 Byron Gnz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ColeccionDePaises{
+    let paises = ["Mexico", "USA", "Canada", "Francia", "Alemania", "Luxemburgo", "Kazajistan", "Italia", "UK", "Colombia", "Venezuela", "Brasil", "Congo", "Australia", "Andorra", "Japon", "Taiwan", "Sudafrica", "Marruecos", "Chile", "Portugal", "Finlandia", "Rusia", "Noruega", "Estonia"]
+    
+    func obtenPais() -> String {
+        let x = paises.count
+        let pais = Int(arc4random()) % x
+        return paises[pais]
+    }
+}
+
+class ColeccionDeHamburguesa{
+    let hamburguesas = ["Mexicanburguer", "USAburguer", "Canadianburguer", "Frenchburguer", "Deutchburguer", "Luxemburgoburguer", "Kazajistanburguer", "Italianburguer", "UKburguer", "Colombiaburguer", "Venezuelaburguer", "Brasilburguer", "Congoburguer", "Australiaburguer", "Andorraburguer", "Japonburguer", "Taiwanburguer", "Sudafricaburguer", "Marruecosburguer", "Chileburguer", "Portugalburguer", "Finlandiaburguer", "Rusiaburguer", "Noruegaburguer", "Estoniaburguer"]
+    
+    func obtenHamburguesa() -> String {
+        let x = hamburguesas.count
+        let burger = Int(arc4random()) % x
+        return hamburguesas[burger]
+    }
+}
+
+struct Colores{
+    let colores = [ UIColor(red:210/255.0, green: 90/255.0, blue: 45/255.0, alpha: 1),
+                    UIColor(red:40/255.0, green: 170/255.0, blue: 45/255.0, alpha: 1),
+                    UIColor(red:3/255.0, green: 180/255.0, blue: 90/255.0, alpha: 1),
+                    UIColor(red:210/255.0, green: 190/255.0, blue: 5/255.0, alpha: 1),
+                    UIColor(red:120/255.0, green: 120/255.0, blue: 50/255.0, alpha: 1),
+                    UIColor(red:130/255.0, green: 80/255.0, blue: 90/255.0, alpha: 1),
+                    UIColor(red:130/255.0, green: 130/255.0, blue: 130/255.0, alpha: 1),
+                    UIColor(red:3/255.0, green: 50/255.0, blue: 90/255.0, alpha: 1)]
+    
+    func regresaColorAleatorio() -> UIColor {
+        let posicio = Int (arc4random()) % colores.count
+        return colores[posicio]
+    }
+}
--- Hamburgesas en el mundo/ViewController.swift
+++ Hamburgesas en el mundo/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let paises = ColeccionDePaises()
+    let burguers = ColeccionDeHamburguesa()
+    let colores = Colores()
+    
+    @IBOutlet weak var etiquetaDelPais: UILabel!
+    @IBOutlet weak var etiquetaBurger: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +27,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func quieroUnaBurguer(_ sender: AnyObject) {
+        let colorAleatorio = colores.regresaColorAleatorio()
+        etiquetaDelPais.text = paises.obtenPais()
+        etiquetaBurger.text = burguers.obtenHamburguesa()
+        view.backgroundColor = colorAleatorio
+       // view.tintColor =
+    }
 
 }
 
